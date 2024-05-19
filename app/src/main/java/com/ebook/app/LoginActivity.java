f7d0baa53a17a22dff39a80d4c9f2ca967811ca7
wilson <wilson_lyc@foxmail.com>
2024-05-19 20:08:23 +0800

    :art: 新增“获取验证码”倒计时

--- app/src/main/java/com/ebook/app/LoginActivity.java
+++ app/src/main/java/com/ebook/app/LoginActivity.java
@@ -2,6 +2,9 @@ package com.ebook.app;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView email;//邮箱输入框
+    private TextView captch;//验证码输入框
+    private CheckBox autoRegister;//自动注册协议复选框
+    Button getCaptch;//获取验证码按钮
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +28,60 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //初始化控件
+        email = findViewById(R.id.login_input_email);
+        captch = findViewById(R.id.login_input_captch);
+        autoRegister = findViewById(R.id.login_auto_register);
+        getCaptch = findViewById(R.id.login_button_get_captch);
     }
 
+    /**
+     * “登录”按钮点击事件
+     * @param view 点击的视图
+     */
     public void onLoginClick(View view) {
-        Toast.makeText(this, "登录", Toast.LENGTH_SHORT).show();
+        if(!autoRegister.isChecked()){
+            Toast.makeText(this, "请先同意自动注册协议", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String emailText = email.getText().toString();
+        String captchText = captch.getText().toString();
+        Toast.makeText(this, emailText+" "+captchText, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * “获取验证码”按钮点击事件
+     * @param view 点击的视图
+     */
     public void onGetCaptchClick(View view) {
-        Toast.makeText(this, "获取验证码", Toast.LENGTH_SHORT).show();
+
+        //按钮倒计时60s
+        getCaptchStartCountdown();
+    }
+
+    /**
+     * “获取验证码”按钮开始倒计时
+     */
+    private void getCaptchStartCountdown(){
+        //修改按钮样式，禁用按钮，60s后重新启用
+        getCaptch.setEnabled(false);
+        getCaptch.setBackground(getDrawable(R.drawable.ebook_button_disabled));
+        getCaptch.setText("60s后重新获取");
+        new Thread(() -> {
+            for(int i=59;i>0;i--){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                final int finalI = i;
+                runOnUiThread(() -> getCaptch.setText(finalI+"s后重新获取"));
+            }
+            runOnUiThread(() -> {
+                getCaptch.setEnabled(true);
+                getCaptch.setBackground(getDrawable(R.drawable.ebook_button));
+                getCaptch.setText("获取验证码");
+            });
+        }).start();
     }
 }
