fbee9fba9da6fdfdbfa09f68217e94b50c3523fb
wilson <wilson_lyc@foxmail.com>
2024-05-28 16:34:50 +0800

    新增首页fragment

--- app/src/main/java/com/ebook/app/views/main/MainActivity.java
+++ app/src/main/java/com/ebook/app/views/main/MainActivity.java
@@ -7,13 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.ebook.app.R;
 import com.ebook.app.util.SharedPrefsUtil;
+import com.ebook.app.views.authority.AuthorityPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class MainActivity extends AppCompatActivity {
-    private SharedPrefsUtil prefsUtil;
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewPager = findViewById(R.id.main_view_pager);
+        tabLayout = findViewById(R.id.main_tab_layout);
+        MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(mainPagerAdapter);
+        // 使用TabLayoutMediator来连接TabLayout与ViewPager2
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("首页");
+                    break;
+                case 1:
+                    tab.setText("E宝");
+                    break;
+                case 2:
+                    tab.setText("我的");
+                    break;
+            }
+        }).attach();
     }
 }
